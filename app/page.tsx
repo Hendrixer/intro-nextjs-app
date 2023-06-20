c391b008899bdc3396ccb2f59844020fc3e539b5
Manel Soler <manelsoler@gmail.com>
2023-06-20 10:40:23 +0200

    home routing linksl

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import styles from '../styles/home.module.css'
 
 export default async function Home() {
   return <div className={styles.home}>
     <div>
-      <div>Name</div>
-      <div>blog</div>
-      <div>contacting</div>
+      <div> <h1>Hi, My name is  Manel </h1></div>
+      <div>
+      <Link href= "/blog">Check my blog</Link>
+      </div>
+      <div>
+        <Link href="/contact"> contact me </Link>
+      </div>
     </div>
   </div>
 }
